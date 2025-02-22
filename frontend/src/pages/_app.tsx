92f8ba3baa4895013bc29e4ce2670e50b6e6b97e
Carmine Villarreal <164991041+carmow@users.noreply.github.com>
2025-02-22 15:12:08 -0500

    config debug

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,9 +1,11 @@
 import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import AppProps from "next/app";
+import type { NextComponentType, NextPageContext } from "next";
 import type { ComponentType } from "react";
 
 interface MyAppProps extends AppProps {
-  Component: ComponentType;
+  Component: NextComponentType<NextPageContext, any, any>;
+  pageProps: any;
 }
 
 export default function App({ Component, pageProps }: MyAppProps) {
