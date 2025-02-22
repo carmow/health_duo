28c92dfe979d82cd98d9d043842c44ec2243f10a
Carmine Villarreal <164991041+carmow@users.noreply.github.com>
2025-02-22 13:51:44 -0500

    Home page 1

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <h1>Welcome to Next.js!</h1>
+      <h1>Health Duolingo - improving your wellness one step at a time</h1>
     </div>
   );
 }
