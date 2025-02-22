8fa7120628a4203c5b7c693ca55336ce5e46c9a2
Carmine Villarreal <164991041+carmow@users.noreply.github.com>
2025-02-22 13:47:25 -0500

    Push vercel

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./frontend/src/**/*.{js,ts,jsx,tsx}", // Adjust the path according to your project structure
+    "./frontend/public/index.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
